202c51ee75743b5ae683e3b37db9244619b63d59
orindojeff <orindojeff@gmail.com>
2023-06-15 14:42:38 -0700

    fixed a bug in  100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,18 @@
 #include <unistd.h>
 
-int main(void) {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a specific message to the standard error
+ * without using printf or puts functions
+ *
+ * Return: Always 1 (indicating an error condition)
+ */
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int length = 59;
+
+	write(2, message, length);
+	return (1);
 }
