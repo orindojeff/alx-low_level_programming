a3b90379e3568a4d865d6594547399826c0747e2
orindojeff <orindojeff@gmail.com>
2023-06-15 14:46:01 -0700

    fixed a bug in intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,14 @@
  * main - Entry point of the program
  *
  * Description: Prints a specific message to the standard error
- * without using printf or puts functions
+ *              without using printf or puts functions
  *
  * Return: Always 1 (indicating an error condition)
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *message = "and that piece of art is useful\" - "
+	                "Dora Korpar, 2015-10-19\n";
 	int length = 59;
 
 	write(2, message, length);
