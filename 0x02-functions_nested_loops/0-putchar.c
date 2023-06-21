366dc608b614bbbae1817ada8930c417fb21ac5f
orindojeff <orindojeff@gmail.com>
2023-06-21 13:56:10 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,40 @@
+#include "unistd.h"
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints _putchar followed by a new line
+ *
+ * Return : Always 0 (sucess)
+ */
+int main(void)
+{
+	char text[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	nclude "main.h"
+	/**
+	 *  * main - Entry point
+	 *   *
+	 *    * Description: A program that prints _putchar followed by a new line
+	 *     *
+	 *      * Return : Always 0 (sucess)
+	 *       */
+	int main(void)
+	{
+		char text[8] = "_putchar";
+		int i = 0;
+	
+		for (i = 0; i < 8; i++)
+		{
+				_putchar(text[i]);
+			}
+		_putchar('\n');
+		return (0);
+	}}
+	_putchar('\n');
+	return (0);
+}
